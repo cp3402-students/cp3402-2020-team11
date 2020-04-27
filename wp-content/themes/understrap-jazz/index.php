ee2695ca1f56cb6026d2e7eb3e9bae4d50b9a653
thomasleumann <thomas.leumann@my.jcu.edu.au>
2020-04-27 22:29:27 +1000

    create and test custom index.php

---
+++ wp-content/themes/understrap-jazz/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+<?php if ( is_front_page() && is_home() ) : ?>
+
+<?php endif; ?>
+
+
+<h1> Testing custom index</h1>
+
+<?php
+get_footer();
