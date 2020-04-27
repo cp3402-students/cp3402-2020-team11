18538403f4b2748556372155aa0a064acc8c0e47
thomasleumann <thomas.leumann@my.jcu.edu.au>
2020-04-27 22:49:26 +1000

    update stylesheet directory

--- wp-content/themes/understrap-jazz/functions.php
+++ wp-content/themes/understrap-jazz/functions.php
@@ -3,7 +3,7 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 function my_theme_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style',
-        get_stylesheet_directory_uri() . '/style.css' ,
+        get_stylesheet_directory_uri() . '/css/jazz-theme.css' ,
         array('parent-style'),
         wp_get_theme()->get('Version')
     );
